ec736db52813adec6ec085f6c620196805018e1a
Asmaalafe55 <asmaalafe55@gmail.com>
2024-04-25 15:12:23 +0300

    inheritance

---
+++ project1/src/Car.java
@@ -0,0 +1,11 @@
+package project1.src;
+
+public class Car extends Factory {
+    // Car class inherites every thing from Factory class
+
+    public void brand(){
+        System.out.println("Tesla");
+    }
+
+    
+}
---
+++ project1/src/Factory.java
@@ -0,0 +1,11 @@
+package project1.src;
+
+public class Factory {
+    
+    public void design(){
+        System.out.println("Design the car");
+    }
+    public void build(){
+        System.out.println("Build the car");
+    }
+}
