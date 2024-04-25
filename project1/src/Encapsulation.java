c954d19e8043030a7e67b5f04ac7c944f3105c5a
Asmaalafe55 <asmaalafe55@gmail.com>
2024-04-25 20:44:42 +0300

    learning Encapsulation

---
+++ project1/src/Encapsulation.java
@@ -0,0 +1,30 @@
+package project1.src;
+
+class Car1{
+    public static final int speed =150;
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getRevenueData(){
+        String data = "Revenue: " + calculateRevenue();
+        return data;
+    }
+
+    private int calculateRevenue(){
+        return 12000;
+    }
+
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        
+    }
+}
