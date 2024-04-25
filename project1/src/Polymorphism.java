cc18ac74e114afb8f4a98515416c2785fda32c15
Asmaalafe55 <asmaalafe55@gmail.com>
2024-04-25 19:53:33 +0300

    uncomminting

--- project1/src/Polymorphism.java
+++ project1/src/Polymorphism.java
@@ -7,11 +7,11 @@ public class Polymorphism {
         Person person1 = new Person();
         Scholar scholar1 = new Scholar();
 
-        // Person person2 = person1;
-        // person2.eyes(); // This calls the eyes() method from Person class
+        Person person2 = person1;
+        person2.eyes(); // This calls the eyes() method from Person class
 
-        Person person2 = scholar1; // points to student object
-        person2.eyes();
+        Person person3 = scholar1; // points to student object
+        person3.eyes();
         // ((Student) person3).eyes(); // Cast person3 back to Student to access eyes() method
     }
 }
