dcd6d91887aa19d1b8c20b73f6620fea246d482a
Asmaalafe55 <asmaalafe55@gmail.com>
2024-04-26 23:42:34 +0300

    trying the multiple exceptions

--- project3/MultipleExceptions/Application.java
+++ project3/MultipleExceptions/Application.java
@@ -1,3 +1,35 @@
 package project3.MultipleExceptions;
 
-public class Application {}
+import java.text.ParseException;
+import javax.imageio.IIOException;
+
+public class Application {
+
+  public static void main(String[] args) {
+    // here we will handle the exceptions as we said before
+
+    MultipleExceptions me = new MultipleExceptions();
+
+    // try {
+    //   me.start();
+    // } catch (IIOException e) {
+    //   e.printStackTrace();
+    // } catch (ParseException e) {
+    //   System.out.println("Incorrect Format");
+    // }
+
+    // try {
+    //   me.start();
+    // } catch (IIOException | ParseException e) {
+    //   e.printStackTrace();
+    // }
+
+    // polemorphism
+    try {
+      me.start();
+    } catch (Exception e) {
+      // it catchs any type of exceptions
+      e.printStackTrace();
+    }
+  }
+}
--- project3/MultipleExceptions/MultipleExceptions.java
+++ project3/MultipleExceptions/MultipleExceptions.java
@@ -1,3 +1,18 @@
 package project3.MultipleExceptions;
 
-public class MultipleExceptions {}
+import java.text.ParseException;
+import javax.imageio.IIOException;
+
+public class MultipleExceptions {
+
+  public void start() throws IIOException, ParseException {
+    // lets say that we have an error
+    throw new IIOException(null, null);
+
+    // lets assume that we have another error
+    // we want to throw another exception
+
+    throw new ParseException(null, 0);
+    // this is not a list of exceptions that are going to be thown, its one exception at a time
+  }
+}
