d7c1c7ee9733adeb1d2c2d09221dc0b742e4f1c7
Asmaalafe55 <asmaalafe55@gmail.com>
2024-05-28 21:54:13 +0300

    serialization

--- project3/Serialization/Reading.java
+++ project3/Serialization/Reading.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class Reading {
 
@@ -14,6 +15,24 @@ public class Reading {
       ObjectInputStream objectInput = new ObjectInputStream(fileInput);
       Vehicle vehicle1 = (Vehicle) objectInput.readObject();
       Vehicle vehicle2 = (Vehicle) objectInput.readObject();
+
+      Vehicle[] vehicle = (Vehicle[]) objectInput.readObject();
+      ArrayList<Vehicle> vehicleList = (ArrayList<
+          Vehicle
+        >) objectInput.readObject();
+
+      for (Vehicle myVehicle : vehicle) {
+        System.out.println(myVehicle);
+      }
+      for (Vehicle myVehicle : vehicleList) {
+        System.out.println(myVehicle);
+      }
+
+      int value = objectInput.readInt();
+      for (int n = 0; n < value; n++) {
+        Vehicle vehicles = (Vehicle) objectInput.readObject();
+        System.out.println(vehicles);
+      }
 
       System.out.println(vehicle1);
       System.out.println(vehicle2);
--- project3/Serialization/Writing.java
+++ project3/Serialization/Writing.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 // serialization means turnning an object into bitstream
 // and if you deserialize an object, it means taking bitstream and turning it back into an object
@@ -16,6 +18,16 @@ public class Writing {
     Vehicle car = new Vehicle(329, "Toyota");
     Vehicle truck = new Vehicle(456, "Ford");
 
+    Vehicle[] vehicle = {
+      new Vehicle(1, "Ford"),
+      new Vehicle(2, "Toyota"),
+      new Vehicle(3, "Fiat"),
+    };
+
+    ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>(
+      Arrays.asList(vehicle)
+    );
+
     System.out.println(car);
     System.out.println(truck);
 
@@ -25,6 +37,13 @@ public class Writing {
       );
       objectOutputStream.writeObject(car); // serialize the object to file / writing
       objectOutputStream.writeObject(truck);
+      objectOutputStream.writeObject(vehicle);
+      objectOutputStream.writeObject(vehicleList);
+
+      objectOutputStream.writeInt(vehicleList.size());
+      for (Vehicle vehicles : vehicleList) {
+        objectOutputStream.writeObject(vehicles);
+      }
 
       objectOutputStream.close();
     } catch (FileNotFoundException e) {
