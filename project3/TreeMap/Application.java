b0a94fafe3081aeb184a98df060365c2078e13a6
Asmaalafe55 <asmaalafe55@gmail.com>
2024-06-04 14:08:13 +0300

    tree map

---
+++ project3/TreeMap/Application.java
@@ -0,0 +1,11 @@
+package project3.TreeMap;
+
+// import java.util.TreeMap;
+
+public class Application {
+
+  public static void main(String[] args) {
+    // TreeMap<Integer, String> myMap = new TreeMap<Integer, String>();
+    // it is the same syntax like the hashMap
+  }
+}
